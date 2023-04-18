9e25481bbd67930c340f41e72f747c4a7f04d6fd
sx9dev <sx-91@outlook.com>
2023-04-18 11:14:14 +0700

    switches

--- boot.asm
+++ boot.asm
@@ -1,16 +1,54 @@
 mov ah, 0x0e
+jmp PrintX
+
+; ======================
+
+PrintX:
+
+mov al, 'X'
+int 0x10
+
+jmp $
+
+; ======================
+
+PrintAZ: 
+
 mov al, 65
 int 0x10
 
 loop:
   inc al
   cmp al, 'Z' + 1
-  je exit
+  je $
   int 0x10
   jmp loop
 
-exit:
+; ======================
+
+PrintYOrN:
+
+mov bx, 1
+cmp bx, 2
+je itsTrue
+jne itsFalse
+jmp $
+
+itsTrue:
+
+  mov al, 'Y'
+  int 0x10
   jmp $
+
+itsFalse:
+
+  mov al, 'N'
+  int 0x10
+  jmp $
+
+; ======================
+
+; Required Code:
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
